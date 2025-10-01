76b0555e7e29f91950a58918f09759e0dba0a81f
TTCGit <tasca.tiberiu@gmail.com>
2025-10-01 23:07:25 +0300

    Finish project

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
   return (
-    <main className="bg-egg ">
+    <main className="bg-egg min-h-screen">
       <div className="container px-20 md:px-0  mx-auto py-40 grid grid-cols-1  xl:grid-cols-[2fr_1fr] gap-8">
         {children}
       </div>
